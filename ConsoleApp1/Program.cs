c0a2204fd82675ccbf6c9f701676fa6f8b483cea
yang.lei <yang.lei@nuance.com>
2020-06-17 12:47:15 +0800

    CharsRange2 3

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -8,20 +8,20 @@ namespace ConsoleApp1
 {
     class Program
     {
-        static IList<string> baseChars = new List<string>() { "0", "1", "2", "3" };
+        static IList<char> baseChars = new List<char>() { '0', '1', '2', '3' };
 
         static void CharsRange1(string prefix, int pos)
         {
             if (pos == 1)
             {
-                foreach (string s in baseChars)
+                foreach (char s in baseChars)
                 {
                     Console.WriteLine(prefix + s);
                 }
             }
             else
             {
-                foreach (string s in baseChars)
+                foreach (char s in baseChars)
                 {
                     CharsRange1(prefix + s, pos - 1);
                 }
@@ -30,25 +30,48 @@ namespace ConsoleApp1
 
         static IEnumerable<string> CharsRange2(string prefix, int pos)
         {
-            if (pos == 1)
+            foreach (char c in baseChars)
             {
-                foreach (string s in baseChars)
+                if (pos == 1)
                 {
-                    yield return prefix + s;
+                    yield return prefix + c;
+                }
+                else
+                {
+                    foreach (string s in CharsRange2(prefix + c, pos - 1))
+                    {
+                        yield return s;
+                    }
                 }
             }
-            else
+        }
+
+        static IEnumerable<string> CharsRange3(char[] strChars, int pos)
+        {
+            foreach (char c in baseChars)
             {
-                foreach (string s in baseChars)
+                strChars[pos - 1] = c;
+                if (pos == 1)
                 {
-                    CharsRange2(prefix + s, pos - 1);
+                    yield return new string(strChars);
+                }
+                else
+                {
+                    foreach (string s in CharsRange3(strChars, pos - 1))
+                    {
+                        yield return s;
+                    }
                 }
             }
         }
         static void Main(string[] args)
         {
             //CharsRange1("", 3);//working
-            foreach (string s in CharsRange2("", 3))
+            //foreach (string s in CharsRange2("", 3))
+            //{
+            //    Console.WriteLine(s);//nothing
+            //}
+            foreach (string s in CharsRange3(new char[] { ' ', ' ', ' ' }, 3))
             {
                 Console.WriteLine(s);//nothing
             }
